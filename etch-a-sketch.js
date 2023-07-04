6aa067eddc4716f9548bda6733f3429eae9fc78e
squendle <squendle@gmail.com>
2023-07-03 17:44:03 -0700

    finish grid creation function - success!

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -10,14 +10,17 @@ const container = document.querySelector(".eas-container");
 // write a function to create a 16x16 grid of divs inside of the container
 
 function createGrid(squares) {
-  const square = document.createElement("div");
+  let square;
   let size = (960 / squares);
+  
   for (let i = 1; i <= squares; i++){
-    for (let i = 1; i <= squares; i++) {
-     square.setAttribute('style', `border: 1px solid blue; height: ${size}px; width: ${size}px`);
-     container.appendChild(square);
+    for (let j = 1; j <= squares; j++) {
+      square = document.createElement("div");
+      square.setAttribute('style', `border: 1px solid blue; height: ${size}px; width: ${size}px`);
+      container.appendChild(square);
     }
   }
+
 };
 
 // then this should put the 16x16 grid in the container...
